47a2b3e779434159eab81b22cd68c6e797d566f4
Sanjana Kushwaha <gssanjanakush@gmail.com>
2022-06-29 17:02:56 +0530

    first

---
+++ src/components/Cart/Cart.jsx
@@ -0,0 +1,89 @@
+import React from "react";
+import { Container, Typography, Button, Grid } from "@material-ui/core";
+import useStyles from "./styles";
+import CartItem from "./CartItem/CartItem";
+import { Link } from "react-router-dom";
+
+const Cart = ({
+  cart,
+  handleUpdateCartQty,
+  handleRemoveFromCart,
+  handleEmptyCart,
+}) => {
+  const classes = useStyles();
+  const EmptyCart = () => (
+    <Typography variant="subtitle1">
+      You have no items in your shopping cart,
+      <Link to="/" className={classes.link}>
+        start adding some
+      </Link>
+    </Typography>
+  );
+  const FilledCart = () => (
+    <>
+      <Grid container spacing={3}>
+        {cart.line_items.map((item) => (
+          <Grid item xs={12} sm={4} key={item.id}>
+            <CartItem
+              item={item}
+              onUpdateCartQty={handleUpdateCartQty}
+              onRemoveFromCart={handleRemoveFromCart}
+            />
+          </Grid>
+        ))}
+      </Grid>
+      <div className={classes.cardDetails}>
+        <Typography variant="h4">
+          SubTotal: {cart.subtotal.formatted_with_symbol}
+        </Typography>
+        <div>
+          <Button
+            className={classes.emptyButton}
+            size="large"
+            type="button"
+            variant="contained"
+            style={{
+              borderRadius: 15,
+              backgroundColor: "#fd7f2c",
+              color: "white",
+              padding: "10px 20px",
+              fontSize: "18px",
+            }}
+            onClick={handleEmptyCart}
+          >
+            Empty cart
+          </Button>
+          <Button
+            component={Link}
+            to="/checkout"
+            className={classes.checkoutButton}
+            size="large"
+            type="button"
+            variant="contained"
+            style={{
+              borderRadius: 15,
+              backgroundColor: "#2eb62c",
+              color: "white",
+              padding: "10px 20px",
+              fontSize: "18px",
+            }}
+          >
+            Checkout
+          </Button>
+        </div>
+      </div>
+    </>
+  );
+  if (!cart.line_items) return "Loading...";
+  return (
+    <Container>
+      <div className={classes.toolbar} />
+      <Typography className={classes.title} variant="h3" gutterBottom>
+        Your shopping cart
+      </Typography>
+      {!cart.line_items.length ? <EmptyCart /> : <FilledCart />}
+    </Container>
+  );
+};
+
+export default Cart;
